a95f0180b10ea9a19f1843188945e65aa6387012
JustTrust <a.belichenko@gmail.com>
2016-03-13 18:56:45 +0200

    added service and gradle import

--- app/src/main/java/org/belichenko/a/onehundredmeterchat/RecyclerViewAdapter.java
+++ app/src/main/java/org/belichenko/a/onehundredmeterchat/RecyclerViewAdapter.java
@@ -29,7 +29,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_item, parent, false);
+                .inflate(R.layout.fragment_list, parent, false);
         return new ViewHolder(view);
     }
 
