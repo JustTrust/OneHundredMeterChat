bed48476341f198041f78cc1666035406bca3f05
Alex <shutCool21@gmail.com>
2016-03-14 14:54:00 +0200

    comit7

--- app/src/main/java/org/belichenko/a/onehundredmeterchat/MapFragment.java
+++ app/src/main/java/org/belichenko/a/onehundredmeterchat/MapFragment.java
@@ -7,17 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  *
  */
-public class MapFragment extends Fragment implements Constant{
+public class MapFragment extends Fragment implements Constant, OnMapReadyCallback {
     private static MapFragment ourInstance = new MapFragment();
+
+    public MapFragment() {
+    }
 
     public static MapFragment getInstance() {
         return ourInstance;
-    }
-
-    public MapFragment() {
     }
 
     @Nullable
@@ -25,5 +30,11 @@ public class MapFragment extends Fragment implements Constant{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //  Marker marker = googleMap.addMarker(new MarkerOptions().position().title())
+
     }
 }
