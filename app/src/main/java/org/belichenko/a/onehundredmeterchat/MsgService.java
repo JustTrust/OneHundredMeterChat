355ddbdf3ef5985c10d341bd9e8a1714a32429cb
Alex <shutCool21@gmail.com>
2016-03-15 14:10:03 +0200

    comit10

--- app/src/main/java/org/belichenko/a/onehundredmeterchat/MsgService.java
+++ app/src/main/java/org/belichenko/a/onehundredmeterchat/MsgService.java
@@ -2,9 +2,43 @@ package org.belichenko.a.onehundredmeterchat;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 
+import java.util.Date;
+
 public class MsgService extends Service {
+    private LocationManager locationManager;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                //gps.setText("Status: "+String.valueOf(status))
+            } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                //net.setText("Status: "+String.valueOf(status))
+            }
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnable();
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnable();
+        }
+    };
+
     public MsgService() {
     }
 
@@ -17,10 +51,37 @@ public class MsgService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    private void showLocation(Location location) {
+        if (location == null) {
+            return;
+        }
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            //Выводим данные с gps gpsText.setText(formatLocation(location));
+        } else if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
+            //Выводим данные с network networkText.setText(formatLocation(location));
+        }
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null) {
+            return "";
+        }
+
+        return String.format("Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT", +
+                location.getLatitude(), location.getLongitude(), new Date(location.getTime()));
+
+    }
+
+    private void checkEnable() {
+        //gps.setText("Enable: "+locationManager.isProviderEnabled(locationManager.GPS_PROVIDER));
+        //net.setText("Enable: "+locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER));
     }
 }
